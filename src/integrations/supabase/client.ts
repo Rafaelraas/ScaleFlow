7503bbd3082a6124d4b0682ecce00b6bfb4e39e7
raasrafael <azeraf@gmail.com>
2025-09-25 01:56:41 -0300

    Updated src/integrations/supabase/client.ts

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -1,6 +1,13 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+//const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
+const supabaseUrl = 'https://ttgntuaffrondfxybxmi.supabase.co'
+//const supabaseKey = process.env.SUPABASE_KEY
+
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+
+
+//const supabase = createClient(supabaseUrl, supabaseKey)
